eee67a848023ae0b5a77c48be31a37ebe77f5a0a
pdh1124 <pdw3231@naver.com>
2021-04-11 23:52:49 +0900

    설명 추가

--- prac_spring_mvc01/src/test/java/kr/icia/persistence/TimeMapperTests.java
+++ prac_spring_mvc01/src/test/java/kr/icia/persistence/TimeMapperTests.java
@@ -19,7 +19,7 @@ public class TimeMapperTests {
 	@Setter(onMethod_ = @Autowired)
 	private TimeMapper timeMapper; // 멤버변수
 	// @Setter : setter 메소드 자동 생성.
-	// @Autowired : 생성된 객체 자동 연결.( ** )
+	// @Autowired : 생성된 객체 자동 연결.매개변수로들어간 setTimeMapper(TimeMapper)의 매개변수 TimeMapper를 초기화시켜서 연결시켜 준다. 
 
 	// 점검 삼아서 구동해볼 메소드라고 알려줌.
 	@Test
