77b14aeca88a5ad777f1b9ac50206001d0a6a2d1
pdh1124 <pdw3231@naver.com>
2021-04-11 23:01:06 +0900

    주석 정리

--- prac_spring_mvc01/src/test/java/kr/icia/persistence/TimeMapperTests.java
+++ prac_spring_mvc01/src/test/java/kr/icia/persistence/TimeMapperTests.java
@@ -2,25 +2,28 @@ package kr.icia.persistence;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import kr.icia.mapper.TimeMapper;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 //현재 클래스는 테스트를 위한 클래스이다.
 @RunWith(SpringJUnit4ClassRunner.class)
-//( ** )
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
-//( ** )
 @Log4j
-public class TimeMapperTests {	
-	
-	private TimeMapper timeMapper; //멤버변수
-	
+public class TimeMapperTests {
+
+	@Setter(onMethod_ = @Autowired)
+	private TimeMapper timeMapper; // 멤버변수
+	// @Setter : setter 메소드 자동 생성.
+	// @Autowired : 생성된 객체 자동 연결.( ** )
+
 	// 점검 삼아서 구동해볼 메소드라고 알려줌.
 	@Test
-	public void testGetTime() { //메소드
+	public void testGetTime() { // 메소드
 		log.info(timeMapper.getTime());
 	}
 }
