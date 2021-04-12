66a0e9b3858483794fc3120b7b63d3912aa64fb5
pdh1124 <pdw3231@naver.com>
2021-04-12 16:32:21 +0900

    테스트로 글쓰기 읽기 삭제 기능 제현

--- prac_spring_mvc01/src/main/java/kr/icia/mapper/BoardMapper.java
+++ prac_spring_mvc01/src/main/java/kr/icia/mapper/BoardMapper.java
@@ -9,4 +9,16 @@ public interface BoardMapper {
 	// List는 ArrayList 부모 클래스,
 	// 가변형 객체 배열 느낌.
 	// index를 가지고 있음.
+	
+	public void insert(BoardVO board); //글쓰기
+	//bno 는 시퀀스 자동 생성으로 나머지 값만 입력.
+	
+	public void insertSelectKey(BoardVO board); //글쓰고 글번호 받기
+	// 생성되는 시퀀스 값을 확인하고 나머지 값 입력.
+	
+	public BoardVO read(Long bno); //읽기
+	
+	public int delete(Long bno); //삭제
+	
+	public int update(BoardVO board); //수정
 }
--- prac_spring_mvc01/src/test/java/kr/icia/mapper/BoardMapperTests.java
+++ prac_spring_mvc01/src/test/java/kr/icia/mapper/BoardMapperTests.java
@@ -1,20 +1,64 @@
 package kr.icia.mapper;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import lombok.Setter;
+import lombok.extern.log4j.Log4j;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
 public class BoardMapperTests {
 	@Setter(onMethod_ = @Autowired)
 	private BoardMapper mapper;
 	
+//	@Test
+//	public void testGetList() {
+//		mapper.getList().forEach(board -> log.info(board));
+//		// 향상된 for : 배열과 배열 1개를 담을 변수
+//		// 담다식 : -> 를 기준으로 좌항은 전달값, 우항은 처리
+//		// 목록은 게시물 여러개(배열), 그중 1개를 board에 담은 다음에 해당 내용을 로그로 출력. 배열 원소가 끝날때 까지 반복
+//		// 기술의 발달로 복잡한 코드가 간단해진 결과로 이해
+//	}
+	
+//	@Test
+//	public void testInsert() {
+//		BoardVO board = new BoardVO();	
+//		board.setTitle("새로 작성하는 글 - 0412");
+//		board.setContent("새로 작성하는 내용 - 0412");
+//		board.setWriter("새로운 작성자 - 0412");
+//		
+//		mapper.insert(board);
+//		log.info(board);
+//		
+//	}
+	
+//	@Test
+//	public void testInsertSelectKey() {
+//		BoardVO board = new BoardVO();
+//		board.setTitle("새로 작성하는 글 SelectKey");
+//		board.setContent("새로 작성하는 내용 SelectKey");
+//		board.setWriter("새로운 작성자 SelectKey");
+//		
+//		mapper.insertSelectKey(board);
+//		log.info(board);
+//	}
+	
+//	@Test
+//	public void testRead() {
+//		BoardVO board = mapper.read(9L);
+//		// L은 bno가 long 타입이라는 것을 알림
+//		
+//		log.info(board);
+//	}
+	
 	@Test
-	public void testGetList() {
-		mapper.getList().forEach(board -> log.info(board));
-		// 향상된 for : 배열과 배열 1개를 담을 변수
-		// 담다식 : -> 를 기준으로 좌항은 전달값, 우항은 처리
-		// 목록은 게시물 여러개(배열), 그중 1개를 board에 담은 다음에 해당 내용을 로그로 출력. 배열 원소가 끝날때 까지 반복
+	public void testDelete() {
+		log.info("delete cnt: " + mapper.delete(7L));
+		//7번 게시물 삭제
 	}
 }
